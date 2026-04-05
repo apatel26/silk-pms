1e5bc380bd4827fdc837ca5dec222a04ff48b8ea
Anish Patel <anishpatel@Patels-Mac-Mini.local>
2026-04-04 21:26:09 -0500

    Exclude api/settings from middleware

--- middleware.ts
+++ middleware.ts
@@ -34,5 +34,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api/auth|api/settings|_next/static|_next/image|favicon.ico).*)'],
 };
